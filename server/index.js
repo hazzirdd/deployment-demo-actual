0013a77657fa2bb0916ea5397c654b71bf66a18f
Hayden Sweet <haydensweet22@gmail.com>
2022-05-18 11:33:29 -0600

    project ready for deployment

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Let's build the path to our front end files so Heroku can access them
+
+// Endpoints
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+})
+
+//When heroku deploys our app, it will provide its own port number.
+//This port number is stored on process.env.PORT.
+
+//IF we are not deploying on Heroku (working on our local machine),
+//it will then default to port 4--5 (i just picked 4005 arbitrarily).
+
+const port = process.env.PORT || 4005;
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+})
